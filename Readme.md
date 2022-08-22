824b2a7302daa2529120dcad9aba0ca571112076
dev.robsonsoares@gmail.com <dev.robsonsoares@gmail.com>
2022-08-22 08:23:15 -0300

    primeiro commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#Github
+
+- git init
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello word 9');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  tupeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+});
+server.listen(4003).then(({ url }) => {
+  console.log(`Server Listening on url ${url}`);
+});
